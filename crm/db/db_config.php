f90af8b42472dc1f836d81bd3409c5e40c87b185
MadushankaSampath <madushanka@nvision.lk>
2022-10-18 13:30:40 +0530

    ts commit

--- crm/db/db_config.php
+++ crm/db/db_config.php
@@ -3,5 +3,5 @@ define('WEBSITE_TITLE','WiBIP');
 define('DB_SERVER','localhost');
 define('DB_SERVER_USERNAME','crm');
 define('DB_SERVER_PASSWORD','arrisportal');
-define('DB_DATABASE','crm_portal'); //bi_portal
+define('DB_DATABASE','crm_portal');
 ?>
