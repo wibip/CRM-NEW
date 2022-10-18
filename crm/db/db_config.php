9a9495f17981d6d55ac867389b24c80ac7c24596
Saman Jayaweera <samanj@nvision.lk>
2022-10-18 17:38:09 +0530

    change db config

--- crm/db/db_config.php
+++ crm/db/db_config.php
@@ -1,12 +1,7 @@
 <?php
 define('WEBSITE_TITLE','WiBIP');
-define('DB_SERVER','bi-db');
-define('DB_SERVER_USERNAME','root');
-define('DB_SERVER_PASSWORD','root');
-define('DB_DATABASE','bi_portal_fro');
-
-// define('DB_SERVER','localhost');
-// define('DB_SERVER_USERNAME','root');
-// define('DB_SERVER_PASSWORD','123456');
-// define('DB_DATABASE','crm_live');
+define('DB_SERVER','localhost');
+define('DB_SERVER_USERNAME','crm');
+define('DB_SERVER_PASSWORD','arrisportal');
+define('DB_DATABASE','crm_portal');
 ?>
