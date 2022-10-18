398a3135d69ce5b2a4a96f557a9285601afee6b8
Saman Jayaweera <samanj@nvision.lk>
2022-10-18 16:35:14 +0530

    add ruckus styles

--- crm/layout/ARRIS/index.php
+++ crm/layout/ARRIS/index.php
@@ -1,4 +1,4 @@
 <script type="text/javascript" src="layout/<?php echo $camp_layout; ?>/js/respo.js?v=3"></script>
-<link id href="layout/<?php echo $camp_layout; ?>/css/respo.css?v=31" rel="stylesheet">
+<link id href="layout/<?php echo $camp_layout; ?>/css/respo.css?v=32" rel="stylesheet">
 
 <link rel="shortcut icon" href="layout/<?php echo $camp_layout; ?>/img/favicon.ico" type="image/x-icon"/>
