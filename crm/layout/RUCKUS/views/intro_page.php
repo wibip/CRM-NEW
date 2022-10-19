2dfebfb6053f5ff7ac47435ea1a393ae8dcd26f7
ManojNvision <manojc@wifi.lk>
2022-10-19 13:48:25 +0530

    ruckus index

---
+++ crm/layout/RUCKUS/views/intro_page.php
@@ -0,0 +1,58 @@
+<?php
+$script1 = ($script=="campaign_ad")?"campaign":$script;
+$page_img_url = 'layout/'.$camp_layout.'/img/'.$script1.'_page_image.jpg';
+
+
+echo '<div class="intro_page" style="position: relative">';
+echo '<div class="intro_page_txt container"></div>';
+	echo '</div>';
+
+ ?>
+
+ <style type="text/css">
+
+
+ 	.intro_page{
+    margin-top: -41px ;
+	}
+
+	.intro-content p{
+		color: #000000 !important;
+		font-weight: bold !important;
+	}
+ 	.intro_page_txt{
+		    margin: auto;
+		    width: 100%;
+			box-sizing: border-box;
+			text-align: center;
+ 	}
+
+ 	.intro_page_txt h1{
+		font-size: 40px;
+		font-family: 'Montserrat-Bold';
+		color: #191919;
+ 	}
+
+ 	.intro_page_txt h2{
+		font-size: 24px;
+		color: #191919;
+		font-family: 'Montserrat-Regular';
+		line-height: 40px;
+ 	}
+
+ 	
+ 	.intro-content a{
+ 		    font-size: 18px;
+    font-weight: bold;
+        position: absolute;
+    bottom: 40px;
+ 	}
+
+ 	.intro-content{
+ 		position: relative;
+ 	}
+
+ 	
+
+ </style>
+
