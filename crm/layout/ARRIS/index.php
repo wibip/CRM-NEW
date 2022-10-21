8c326978d528913e02744177bef3370f1ca3d3c8
Isuru Sanjana <isurusanjana@gmail.com>
2022-10-21 09:24:42 +0530

    changes of cleaning the code

---
+++ crm/layout/ARRIS/index.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="layout/<?php echo $camp_layout; ?>/js/respo.js?v=3"></script>
+<link id href="layout/<?php echo $camp_layout; ?>/css/respo.css?v=34" rel="stylesheet">
+
+<link rel="shortcut icon" href="layout/<?php echo $camp_layout; ?>/img/favicon.ico" type="image/x-icon"/>
