bc55235da8e31a3879938593ec066b8f674e961c
Isuru Sanjana <isurusanjana@gmail.com>
2022-11-15 10:37:24 +0530

    checking the OID

---
+++ OID/cred.php
@@ -0,0 +1,6 @@
+<?php
+
+$issuer = 'https://auth.k8spre.arriswifi.com';
+$cid = 'oidc-wibip-client';
+$secret = '0icV/gY8fSEWLF6GBHItcZfvnFaahwPXps8BJMucaxk=';
+$scope = array('openid','profile','email','groups');
