14550afeace673c8059f50c05f96c43acdfa9753
Isuru Sanjana <isurusanjana@gmail.com>
2022-11-16 09:16:02 +0530

    changes related to OID

--- OID/index.php
+++ OID/index.php
@@ -9,9 +9,10 @@ $oidc = new Jumbojett\OpenIDConnectClient($issuer, $cid, $secret);
 error_log('0');
 // added by john - this is the scope that other apps are using, you need to send groups to receive them
 $oidc->addScope($scope);
-var_dump($oidc);die;
+
 error_log('1');
 $oidc->authenticate();
+var_dump($oidc);die;
 error_log('2');
 $oidc->requestUserInfo('sub');
 error_log('3');
