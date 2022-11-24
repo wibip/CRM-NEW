2bf485669954183949ff78e90f2fbfdefe43d543
Isuru Sanjana <isurusanjana@gmail.com>
2022-11-24 11:06:36 +0530

    chanes related to crm simple form

--- crm/classes/CommonFunctions.php
+++ crm/classes/CommonFunctions.php
@@ -29,11 +29,14 @@ class CommonFunctions{
         return $gap;
     }
 
-    public static function httpGet($url){
+    public static function httpGet($url,$json=false){
         $ch = curl_init();
          curl_setopt($ch,CURLOPT_URL,$url);
          curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-         //  curl_setopt($ch,CURLOPT_HEADER, false);
+         
+        if($json==true) {
+            curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        }
 
          $output=curl_exec($ch);
 
@@ -370,7 +373,7 @@ class CommonFunctions{
         $authorization = "Authorization: Bearer ".$token; // Prepare the authorisation token
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization )); // Inject the token into the header
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
         $output=curl_exec($ch);
 
         curl_close($ch);
