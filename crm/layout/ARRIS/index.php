292ed9f51f775a0abc91bb2c4f9a1f5839d65e68
Saman Jayaweera <samanj@nvision.lk>
2022-12-02 11:43:13 +0530

    ruckus layout for crm

--- crm/layout/ARRIS/index.php
+++ crm/layout/ARRIS/index.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="layout/<?php echo $camp_layout; ?>/js/respo.js?v=3"></script>
-<link id href="layout/<?php echo $camp_layout; ?>/css/respo.css?v=34" rel="stylesheet">
+<script type="text/javascript" src="layout/<?php echo $camp_layout; ?>/js/respo.js?v=5"></script>
+<link id href="layout/<?php echo $camp_layout; ?>/css/respo.css?v=35" rel="stylesheet">
 
-<link rel="shortcut icon" href="layout/<?php echo $camp_layout; ?>/img/favicon.ico" type="image/x-icon"/>
+<link rel="shortcut icon" href="layout/<?php echo $camp_layout; ?>/img/favicon.ico?V=2" type="image/x-icon"/>
