bbf5014c9f6c5eb81bdbb9181764bc7f13de6040
Isuru Sanjana <isurusanjana@gmail.com>
2022-12-14 11:11:42 +0530

    changes related to new menu item

--- crm/layout/ARRIS/views/header_navbar.php
+++ crm/layout/ARRIS/views/header_navbar.php
@@ -331,7 +331,7 @@ foreach ($main_mod_array as $keym => $valuem) {
 
 if($_SESSION['SADMIN'] == true && isset($_SESSION['section']) && $_SESSION['section'] != 'ADMIN') {
 // 	echo '<li><a href="./change_portal?section='.$_SESSION["section"].'">Change portal</a></li>';
-	echo '<li><a href="./change_portal?section='.$_SESSION["section"].'"><div><i class="icon-cogs show"></i><span>Change portal&nbsp;&nbsp;</span></div></a></li>';
+	echo '<li><a href="./change_portal?section='.$_SESSION["section"].'"><div><i class="icon-signin show"></i><span>Change portal&nbsp;&nbsp;</span></div></a></li>';
 }
 
 ?>
