2db0c8cf83dfa10c7f914a609c89f13c53610b98
Isuru Sanjana <isurusanjana@gmail.com>
2023-01-09 12:00:22 +0530

    changes related to OID

--- OID/index.php
+++ OID/index.php
@@ -8,6 +8,6 @@ $oidc = new OpenIDConnectClient($issuer, $cid, $secret);
 
 $oidc->providerConfigParam(array('token_endpoint'=>'https://auth.k8spre.arriswifi.com/connect/token'));
 $oidc->addScope($scope);
-
+$oidc->authenticate();
 // this assumes success (to validate check if the access_token property is there and a valid JWT) :
 $clientCredentialsToken = $oidc->requestClientCredentialsToken()->access_token;
