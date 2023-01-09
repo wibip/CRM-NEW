4e90ce6ba290738b03df242a48a6eabab560bba3
Isuru Sanjana <isurusanjana@gmail.com>
2023-01-09 10:46:37 +0530

    changes related to OID

--- OID/index.php
+++ OID/index.php
@@ -6,6 +6,8 @@ require 'cred.php';
 use Jumbojett\OpenIDConnectClient;
 
 $oidc = new OpenIDConnectClient($issuer, $cid, $secret);
+var_dump($oidc);
+die;
 $oidc->providerConfigParam(array('token_endpoint'=>'https://auth.k8spre.arriswifi.com/connect/token'));
 $oidc->addScope($scope);
 
