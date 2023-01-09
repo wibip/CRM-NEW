44e80b84305eb3b81c804717a913eaec5d70c974
Isuru Sanjana <isurusanjana@gmail.com>
2023-01-09 12:58:54 +0530

    changes related to OID

--- OID/cred.php
+++ OID/cred.php
@@ -1,6 +1,6 @@
 <?php
 
-$issuer = 'https://auth.k8spre.arriswifi.com/auth/realms/Infra';
+$issuer = 'https://auth.k8spre.arriswifi.com';
 $cid = 'oidc-wibip-client';
 $secret = '0icV/gY8fSEWLF6GBHItcZfvnFaahwPXps8BJMucaxk=';
 $scope = array('openid','profile','email','groups');
