d8f1b220a33808b540d27cdd3c907961c6e4eb9a
Isuru Sanjana <isurusanjana@gmail.com>
2023-01-10 15:48:57 +0530

    changes related OID

--- OID/index.php
+++ OID/index.php
@@ -6,16 +6,16 @@ require 'cred.php';
 use Jumbojett\OpenIDConnectClient;
 $oidc = new OpenIDConnectClient($issuer, $cid, $secret);
 
-$oidc->setCertPath('./certificate.crt');
-// $oidc->providerConfigParam(array('token_endpoint'=>'https://auth.k8spre.arriswifi.com/connect/token'));
-  
-$oidc->authenticate();
-// this assumes success (to validate check if the access_token property is there and a valid JWT) :
-// $clientCredentialsToken = $oidc->requestClientCredentialsToken()->access_token;
-// $clientCredentialsToken = $oidc->requestResourceOwnerToken(TRUE)->access_token;
-
+/*First attempt- Get user details*/
 // $oidc->setCertPath('./certificate.crt');
+// // $oidc->providerConfigParam(array('token_endpoint'=>'https://auth.k8spre.arriswifi.com/connect/token'));
+// $oidc->authenticate();
+// $name = $oidc->requestUserInfo();
+// var_dump($name);
 
-$name = $oidc->requestUserInfo();
-var_dump($name);
+/*Second attempt register a client*/
+$oidc->register();
+$client_id = $oidc->getClientID();
+$client_secret = $oidc->getClientSecret();
+var_dump($client_secret);
 
