37142224e8c97ad0efcd77137b7f4d01c0be03a9
Isuru Sanjana <isurusanjana@gmail.com>
2023-01-10 17:29:31 +0530

    changes related OID

--- OID/index.php
+++ OID/index.php
@@ -7,18 +7,18 @@ use Jumbojett\OpenIDConnectClient;
 $oidc = new OpenIDConnectClient($issuer, $cid, $secret);
 
 /*First attempt- Get user details*/
-// $oidc->setCertPath('./certificate.crt');
-// // $oidc->providerConfigParam(array('token_endpoint'=>'https://auth.k8spre.arriswifi.com/connect/token'));
-// $oidc->authenticate();
-// $name = $oidc->requestUserInfo();
-// var_dump($name);
+$oidc->setCertPath('./certificate.crt');
+// $oidc->providerConfigParam(array('token_endpoint'=>'https://auth.k8spre.arriswifi.com/connect/token'));
+$oidc->authenticate();
+$name = $oidc->requestUserInfo();
+var_dump($name);
 
 /*Second attempt- Request Client Credentials Token*/
-$oidc->setResponseTypes(array('token'));
-$oidc->addScope(array('openid'));
-$oidc->setAllowImplicitFlow(true);
-$oidc->addAuthParam(array('response_mode' => 'form_post'));
-$oidc->setCertPath('./certificate.crt');
-$oidc->authenticate();
-$sub = $oidc->getVerifiedClaims('sub');
-var_dump($sub);
+// $oidc->setResponseTypes(array('token'));
+// $oidc->addScope(array('openid'));
+// $oidc->setAllowImplicitFlow(true);
+// $oidc->addAuthParam(array('response_mode' => 'form_post'));
+// $oidc->setCertPath('./certificate.crt');
+// $oidc->authenticate();
+// $sub = $oidc->getVerifiedClaims('sub');
+// var_dump($sub);
