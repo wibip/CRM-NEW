d0f8d2d9a1be1e383a6a66ab8b5d09ecc4459d99
Isuru Sanjana <isurusanjana@gmail.com>
2023-01-26 11:28:33 +0530

    Changes related to OID

--- crm/src/auth/local_auth/login.php
+++ crm/src/auth/local_auth/login.php
@@ -33,7 +33,7 @@ if(isset($_POST['sign_in'])){
 		}
 		if($key == 'groups'){
 			$oid_group = $value;
-			echo $value;
+			// echo $value;
 			if($value == 'crm-operations') { // For temporary solution for operations till fix the DB record issue in RM side
 				$oid_group = 'crm-admin';
 			}
@@ -43,8 +43,8 @@ if(isset($_POST['sign_in'])){
 	$checkUserSql = "SELECT COUNT(*) AS f FROM admin_users AS au
 				  	INNER JOIN admin_access_roles AS aar ON aar.access_role=au.access_role 
 				  	WHERE au.user_name='$username' AND aar.oid_group='$oid_group'";
-	echo $checkUserSql;
-	die;
+	// echo $checkUserSql;
+	// die;
 	$checkUserResult = $dbT->selectDB($checkUserSql);
 
 	if($checkUserResult["data"][0]["f"] == 0){
