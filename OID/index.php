ffdc39626bf2a999cf4df59902095f5271942055
Isuru Sanjana <isurusanjana@gmail.com>
2023-01-26 08:28:30 +0530

    Changes related to OID

--- OID/index.php
+++ OID/index.php
@@ -13,31 +13,31 @@ $oidc->setCertPath('./certificate.crt');
 $oidc->addScope($scope);
 // $oidc->providerConfigParam(array('token_endpoint'=>'https://auth.k8spre.arriswifi.com/connect/token'));
 $oidc->authenticate();
-$oidc = $oidc->requestUserInfo();
-var_dump($oidc);
+// $oidc = $oidc->requestUserInfo();
+// var_dump($oidc);
 
 /*Second attempt- Request Client Credentials Token*/
-// $oidc->setResponseTypes(array('token'));
-// $oidc->addScope(array('openid'));
-// $oidc->setAllowImplicitFlow(true);
-// $oidc->addAuthParam(array('response_mode' => 'form_post'));
-// $oidc->setCertPath('./certificate.crt');
-// $oidc->authenticate();
-// $sub = $oidc->getVerifiedClaims('sub');
-// var_dump($sub);
+$oidc->setResponseTypes(array('token'));
+$oidc->addScope(array('openid'));
+$oidc->setAllowImplicitFlow(true);
+$oidc->addAuthParam(array('response_mode' => 'form_post'));
+$oidc->setCertPath('./certificate.crt');
+$oidc->authenticate();
+$sub = $oidc->getVerifiedClaims('sub');
+var_dump($sub);
 
-$session = array();
-foreach($oidc as $key=> $value) {
-    if(is_array($value)) {
-	    $v = implode(', ', $value);
-    } else {
-	    $v = $value;
-    }
-    $session[$key] = $v;
-}
+// $session = array();
+// foreach($oidc as $key=> $value) {
+//     if(is_array($value)) {
+// 	    $v = implode(', ', $value);
+//     } else {
+// 	    $v = $value;
+//     }
+//     $session[$key] = $v;
+// }
 
-session_start();
-$_SESSION['attributes'] = $session;
+// session_start();
+// $_SESSION['attributes'] = $session;
 
 // header("Location: ./attributes.php");
 
