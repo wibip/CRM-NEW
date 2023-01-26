b3a3051314aa5be609a141bf928a962b5c762a4b
Isuru Sanjana <isurusanjana@gmail.com>
2023-01-26 08:30:57 +0530

    Changes related to OID

--- OID/index.php
+++ OID/index.php
@@ -17,14 +17,19 @@ $oidc->authenticate();
 // var_dump($oidc);
 
 /*Second attempt- Request Client Credentials Token*/
-$oidc->setResponseTypes(array('token'));
-$oidc->addScope(array('openid'));
-$oidc->setAllowImplicitFlow(true);
-$oidc->addAuthParam(array('response_mode' => 'form_post'));
-$oidc->setCertPath('./certificate.crt');
-$oidc->authenticate();
-$sub = $oidc->getVerifiedClaims('sub');
-var_dump($sub);
+try{
+    $oidc->setResponseTypes(array('token'));
+    $oidc->addScope(array('openid'));
+    $oidc->setAllowImplicitFlow(true);
+    $oidc->addAuthParam(array('response_mode' => 'form_post'));
+    $oidc->setCertPath('./certificate.crt');
+    $oidc->authenticate();
+    $sub = $oidc->getVerifiedClaims('sub');
+    var_dump($sub);
+} catch(Exception $e){
+    var_dump($e);
+}
+
 
 // $session = array();
 // foreach($oidc as $key=> $value) {
