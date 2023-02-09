8ecac6a6af823963890b92646f039b4e1e8dd70c
Saman Jayaweera <samanj@nvision.lk>
2023-02-09 16:35:32 +0530

    sidebar icons

--- crm/layout/ARRIS/views/header_navbar_new.php
+++ crm/layout/ARRIS/views/header_navbar_new.php
@@ -8,8 +8,8 @@ $icon_arr = [
 	'Logs'=> 'fa-solid fa-inbox',
 	'CRM'=> 'fa-solid fa-people-group',
 	'OPSaaS'=> 'fa-solid fa-people-group',
-	'Clients'=> 'fa-solid fa-people-group',
-	'Properties'=> 'icon-building',
+	'Clients'=> 'fa-solid fa-users-viewfinder',
+	'Properties'=> 'fa-solid fa-building-user',
 	'Admin Config'=> 'fa-solid fa-pen-to-square'
 ];
 if ($script != 'verification') {
