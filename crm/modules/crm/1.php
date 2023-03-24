b7f312e5a5b10f39a1e590a20e956311042e49b3
Isuru Sanjana <isurusanjana@gmail.com>
2023-03-24 18:26:14 +0530

    changes related to order form updates

--- crm/modules/crm/1.php
+++ crm/modules/crm/1.php
@@ -48,7 +48,7 @@ $baseUrl = $apiUrl.'/api/'.$apiVersion;
 
 $data = json_encode(['username'=>$apiUsername, 'password'=>$apiPassword]);
 $tokenReturn = json_decode( $CommonFunctions->httpPost($baseUrl.'/token',$data,true),true);
-var_dump($api_details);
+var_dump($tokenReturn);
 //generating api call to get Service Types
 if($tokenReturn['status'] == 'success') {
     $token = $tokenReturn['data']['token'];
