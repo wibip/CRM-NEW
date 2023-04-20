75b8c502132d1a4f5c2888c9379ef9e307d4b2b4
Isuru Sanjana <isurusanjana@gmail.com>
2023-04-20 18:37:46 +0530

    changes related to new pages

--- crm/assets/js/lib/Guid.js
+++
@@ -1,1 +0,0 @@
-function guidS4(){return(65536*(1+Math.random())|0).toString(16).substring(1)}function generateGuid(){return guidS4()+guidS4()+"-"+guidS4()+"-"+guidS4()+"-"+guidS4()+"-"+guidS4()+guidS4()+guidS4()};
